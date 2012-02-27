38067e2125e790cba4305d20e8b8e83ef78f41ef
Constantine Lignos <lignos@seas.upenn.edu>
2012-02-27 16:02:39 -0500

    Adds simple spec generation stub.

---
+++ ltlbroom/specgeneration.py
@@ -0,0 +1,19 @@
+"""Generates a logical specification from natural language."""
+
+
+def generate(text, sensors, regions, props):
+    """Generate a logical specification from natural language and propositions.""" 
+    responses = []
+    for line in text.split('\n'):
+        print "Parsing:", repr(line)
+        responses.append("Ok!")
+        #parse = parse_text(text)
+        #result = WORLD_KNOWLEDGE.process_parse_tree(parse, text)
+    
+    environment_lines = []
+    system_lines = ["[]<>(s.r1)",
+                    "[]<>(s.r2)"]
+    custom_props = []
+
+    print "Spec generation complete."    
+    return environment_lines, system_lines, custom_props
