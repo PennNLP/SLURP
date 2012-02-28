b724565c1ae992ad606a0ba88605c7d0f94b2796
Constantine Lignos <lignos@seas.upenn.edu>
2012-02-28 15:44:12 -0500

    Adds go and patrol support to spec generation.

--- ltlbroom/specgeneration.py
+++ ltlbroom/specgeneration.py
@@ -7,7 +7,7 @@ from pipelinehost import socket_parse, DEFAULT_PORT
 
 # LTL Constants
 ALWAYS = "[]"
-FINALLY = "<>"
+EVENTUALLY = "<>"
 TO = "->"
 IFF = "<->"
 NOT = "!"
@@ -16,6 +16,15 @@ OR = "|"
 ROBOT_STATE = "s."
 ENV_STATE = "e."
 
+# Semantics constants
+THEME = "Theme"
+LOCATION = "Location"
+PATIENT = "Patient"
+SOURCE = "Source"
+UNDERSPECIFIED = "*"
+
+# Generation constants
+VISIT = "m_visit"
 
 class SpecGenerator(object):
     """Enables specification generation using natural language."""
@@ -29,12 +38,14 @@ class SpecGenerator(object):
             raise IOError("Could not connect to pipelinehost on port %d. "
                           "Make sure that pipelinehost is running." % DEFAULT_PORT)
         self.world_knowledge = Knowledge()
+        
+        # Sets of propositions, accessible to all methods
+        self.custom_props = set()
 
     def generate(self, text, sensors, regions, props):
         """Generate a logical specification from natural language and propositions."""
         # Clean unicode out of everything
-        if isinstance(text, unicode):
-            text = text.encode('ascii', 'ignore')
+        text = text.encode('ascii', 'ignore')
         sensors = [astr.encode('ascii', 'ignore') for astr in sensors]
         regions = [astr.encode('ascii', 'ignore') for astr in regions]
         props = [astr.encode('ascii', 'ignore') for astr in props]
@@ -43,15 +54,22 @@ class SpecGenerator(object):
         print "Sensors:", sensors
         print "Props:", props
         print "Regions:", regions
+        print "Text:", repr(text)
         
         # Make lists for POS conversions
         force_nouns = list(regions) + list(sensors)
         force_verbs = list(props)
         
         responses = []
+        system_lines = []
+        environment_lines = []
+        custom_props = set()
         for line in text.split('\n'):
             if not line:
                 continue
+            
+            # Lowercase and strip the text before using it
+            line = line.strip().lower()
             
             print "Sending to remote parser:", repr(line)
             parse = socket_parse(asocket=self.sock, text=line, force_nouns=force_nouns,
@@ -60,14 +78,52 @@ class SpecGenerator(object):
             user_response, semantics_result, semantics_response, new_commands = \
                 self.world_knowledge.process_parse_tree(parse, line)
             responses.append(user_response)
-        
-        environment_lines = []
-        system_lines = ["[]<>(s.%s)" % region for region in regions]
-        custom_props = []
-    
-        print "Spec generation complete, responses:"
-        print responses
+            
+            # Build the metapars
+            for command in new_commands:
+                new_sys_lines, new_env_lines, new_custom_props = _apply_metapar(command)
+                system_lines.extend(new_sys_lines)
+                new_env_lines.extend(new_env_lines)
+                custom_props.update(new_custom_props)
+
+        # Convert custom props into a list
+        custom_props = list(custom_props)
+
+        print "Spec generation complete."
+        print "Responses:", responses
+        print "Environment lines:", environment_lines
+        print "System lines:", system_lines
+        print "Custom props:", custom_props
         return environment_lines, system_lines, custom_props, responses
+
+
+def _apply_metapar(command):
+    """Generate a metapar for a command."""
+    # ('go', {'Location': 'porch'})
+    name, args = command
+    handler, targets = METAPARS[name]
+    # Extract the targets from args
+    args = [args[target] for target in targets]
+    return handler(*args)
+
+
+def _gen_patrol(region):
+    """Generate a statement to always eventually be in a location."""
+    return (_always_eventually(_sys(region)), [], [])
+            
+def _gen_go(region):
+    """Generate a statement to go to a location once."""
+    return ([_always_eventually(_sys(_have_visited(region))), 
+             _always(_iff(_next(_sys(_have_visited(region))), _or((_next(_sys(region)),
+                                                                  _sys(_have_visited(region))))))],
+            [], [_have_visited(region)])
+
+def _have_visited(region):
+    """Generate a proposition for having visited a region."""
+    return VISIT + '_' + region
+
+# MetaPARS have to be defined after all handler
+METAPARS = {'patrol': (_gen_patrol, (LOCATION,)), 'go': (_gen_go, (LOCATION,))}
 
 
 def _space(text):
@@ -95,9 +151,9 @@ def _always(text):
     return _parens(ALWAYS + _parens(text))
 
 
-def _always_finally(text):
+def _always_eventually(text):
     """Wrap text in always."""
-    return _parens(ALWAYS + FINALLY + _parens(text))
+    return _parens(ALWAYS + EVENTUALLY + _parens(text))
 
 
 def _not(text):
