164fca92367138415f286d9c71ea0162197023de
Constantine Lignos <lignos@seas.upenn.edu>
2012-09-15 00:44:16 -0400

    License on specgeneration.

--- ltlbroom/specgeneration.py
+++ ltlbroom/specgeneration.py
@@ -1,4 +1,24 @@
-"""Generates a logical specification from natural language."""
+"""
+Generates a logical specification from natural language.
+
+"""
+
+# Copyright (C) 2010-2012 Constantine Lignos
+#
+# This file is a part of SLURP.
+#
+# SLURP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# SLURP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SLURP.  If not, see <http://www.gnu.org/licenses/>.
 
 import socket
 
