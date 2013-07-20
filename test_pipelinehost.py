2ea29855df456114220d1fd8666939aa16c5ea8d
Constantine Lignos <lignos@seas.upenn.edu>
2013-07-19 22:34:19 -0400

    Support command line input for test_pipelinehost.

--- test_pipelinehost.py
+++ test_pipelinehost.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-"""An interactive test client for the pipelin host."""
+"""An interactive test client for the pipeline host."""
+
+import sys
 
 from pipelinehost import PipelineClient
 
@@ -7,16 +9,25 @@ from pipelinehost import PipelineClient
 def test():
     """Test by parsing some text."""
     client = PipelineClient(verbose=True)
-    while True:
-        try:
-            text = raw_input('> ')
-        except (KeyboardInterrupt, EOFError):
-            break
-        msg = client.parse(text)
+
+    # If command line arguments are supplied, run them through
+    if len(sys.argv) > 1:
+        msg = client.parse(" ".join(sys.argv[1:]))
         if not msg:
             print "Connection to server closed."
-            break
-        print msg
+        else:
+            print msg
+    else:
+        while True:
+            try:
+                text = raw_input('> ')
+            except (KeyboardInterrupt, EOFError):
+                break
+            msg = client.parse(text)
+            if not msg:
+                print "Connection to server closed."
+                break
+            print msg
 
 
 if __name__ == "__main__":
