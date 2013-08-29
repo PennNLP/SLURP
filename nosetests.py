7fec95eed28a42298150fe513019901a704aca77
Constantine Lignos <lignos@seas.upenn.edu>
2013-08-29 12:13:16 -0400

    Make pipelinehost detection more reliable in tests.

--- nosetests.py
+++ nosetests.py
@@ -6,14 +6,17 @@ import os
 
 import nose
 
-from pipelinehost import PipelineHost
+from pipelinehost import PipelineHost, PipelineClient
 
 # Run the pipelinehost in advance just in case. If there's already a
 # pipeline running, this will just fail without causing any problems.
 try:
+    print "Checking whether PipelineHost is running..."
+    client = PipelineClient()
+except IOError:
     print "Launching PipelineHost..."
     pipeline = PipelineHost(local=True)
-except IOError:
+else:
     print "PipelineHost appears to already be running."
 
 # Put the root of SLURP on the path, just to be safe
