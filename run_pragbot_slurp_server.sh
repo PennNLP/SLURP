f97442e50b9ae882c5f4706cfcc5e6e72026d298
Constantine Lignos <lignos@seas.upenn.edu>
2013-09-01 19:33:57 -0400

    Add error if pragbot server is run outside of LTLMoP submodule.

--- run_pragbot_slurp_server.sh
+++ run_pragbot_slurp_server.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 export PYTHONPATH=../../lib
+if [ ! -d $PYTHONPATH ]; then
+    echo "Error: LTLMoP cannot be imported."
+    echo "This probably means you are not running this script from inside"
+    echo "the SLURP submodule of a LTLMoP respository (/src/etc/SLURP)."
+    exit 1
+fi
 python -u pragbot_slurp_server.py 10007
