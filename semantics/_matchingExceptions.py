17b0c6e5e4e100d376784b1902a1c581f9974e9a
Tad Turpen <tturpen@brandeis.edu>
2013-10-14 18:43:23 -0400

    Matching paths digs to main verb and reverse traverses to fill slots in frame...almost finished

--- semantics/_matchingExceptions.py
+++ semantics/_matchingExceptions.py
@@ -8,5 +8,14 @@ Define custom exceptions for semantics
 class NoSChildVP(Exception):
     pass
 
-class VBTooDeep(Exception):
+class PosTooDeep(Exception):
+    def __init__(self,head):
+        return head
+
+class VerbFrameCountError(Exception):
+    #Raised if the Verb frame has too many or too few verbs
+    pass
+
+class SlotTreeCountError(Exception):
+    #Raised if there are too many slots to be filled given a tree
     pass
--- semantics/matching.py
+++ semantics/matching.py
@@ -4,8 +4,9 @@ parse and tree are synonymous
 @author: tad
 '''
 from nltk import Tree
-from _matchingExceptions import NoSChildVP, VBTooDeep
+from _matchingExceptions import NoSChildVP, PosTooDeep, VerbFrameCountError, SlotTreeCountError
 import sys
+import copy
 DEBUG = False
 def main():
     tests = [exampePPAttachment()]
@@ -34,8 +35,8 @@ class exampePPAttachment(object):
         matcher.th.depth_augment(self.correct,0)
         matcher.th.depth_augment(self.incorrect,0)
         for frame in self.frame_list:
-            cmatch = matcher.match_frame(frame,\
-                                 self.correct)
+            #cmatch = matcher.match_frame(frame,\
+            #                     self.correct)
             icmatch = matcher.match_frame(frame,\
                                  self.incorrect)
 
@@ -76,14 +77,15 @@ class TreeHandler(object):
         '''Recursively returns the node for the leftmost pos'''
         
         if len(tree) == 1:
-            if type(tree[0][0]) == type(''):
-                #If leaf is str
+            #If leaf is str
+            if type(tree[0][0]) == type(''):                
                 depthsplit = tree.node.split(self.depthdelim)#Split on depth delim
                 possplit = depthsplit[0].split(self.posdelim)#split on pos delim
+                #If this is the pos we are looking for
                 if possplit[0] == '' and possplit[1] != '':
-                    if possplit[1] == pos:
+                    if possplit[1] in pos:
                         return tree                   
-                elif possplit[0] == pos:                    
+                elif possplit[0] in pos:                    
                     return tree
             for branch in tree:
                 if type(branch) == type(''):
@@ -97,22 +99,57 @@ class TreeHandler(object):
                 return res
         return None
         
-    def get_main_pos_path(self,tree,pos,maxVBDepth):
+    def get_main_pos_path(self,tree,pos,maxPosDepth):
         '''Return path to the shallowest leftmost VB'''        
         node = self.leftmost_pos(tree, pos)
         path = self.get_path_to_node(tree,node)
-        if len(path) > maxVBDepth:
-            raise VBTooDeep
+        if maxPosDepth != -1 and len(path) > maxPosDepth:            
+            raise PosTooDeep(pos)        
         return path    
         
     def get_path_to_node(self,tree,node):
         leaves = tree.leaves()
-        #accomodate nullsubjects here        
+        #accomodate nullsubjects here
+        node is the phrase, which is good except we are trying to match on leafs for heads, not phrases        
         index = leaves.index(node[0])
         return tree.leaf_treeposition(index)
-     
-        
-
+    
+    def nearest_right_sibling(self,leafpath,tree):
+        '''Returns the path(inclusive) to the branch of the nearest right sibling of leafpath
+            (1,0,1,1,0,0) returns [1,0,1,1] iff the subtree at [1,0,1,1] has > 1 children
+            destructive mthd, makes a copy of tree            
+        '''
+        #destructive mthd, make copy
+        tree = copy.deepcopy(tree)
+        #b for branch, r for reverse node order        
+        path = []
+        for branch in leafpath:
+            tree = tree.pop(branch)
+            path.append((branch,tree))
+        foundSibling = False
+        res = []
+        for branch,r in reversed(path):
+            if foundSibling:
+                res.insert(0,branch)
+            elif len(r) > 1:
+                foundSibling = True
+        return res
+                
+    def nearest_left_sibling(self,leafpath):
+        '''Returns the path(inclusive) to the branch of the nearest left sibling of leafpath
+            (1,0,1,1,0,0) returns [1,0,1,1]
+            (1,0,0) returns [1]
+        '''
+        res = []
+        foundLeft = False
+        for b in reversed(leafpath):
+            if foundLeft:
+                res.insert(0,b)
+            elif b > 0:
+                foundLeft = True 
+                res.insert(0,b)   
+        return res
+    
 class ParseMatcher(object):
     '''
     This class keeps track of state (traversal) information
@@ -128,36 +165,101 @@ class ParseMatcher(object):
         self.strictMatching = smatch
         self.strictPPMatching = sppmatch
         self.th = TreeHandler()
-        self.maxVBDepth = smatch + 3
-        self.vbDepth = -1
-        self.subjectDepth = -1
-        self.directObjectDepth = -1
-        self.indirectObjectDepth = -1
+#         self.maxVBDepth = smatch + 3
+#         self.vbDepth = -1
+#         self.subjectDepth = -1
+#         self.directObjectDepth = -1
+#         self.indirectObjectDepth = -1
         self.role_dict= {
                          'subject' : 1,
                          'verb' : 2,
                          'prep' : 3,
                          'object' : 4
                          }
-        self.posmap = { 'VERB' : 'VB',
-                        'NP' : 'NONE',
-                        'PREP' : {"to towards" : "TO",
-                                  "PREP": "IN"}}       
+        self.pos_map = { 'VERB' : ['VB'],
+                        'NP' : ['NONE','NNS','NN','NNP','NNPS'],
+                        'PREP' : {"to towards" : ["TO"],
+                                  "PREP": ["IN"]}}   
+        self.depth_map = {'maxVB' : smatch + 3,
+                          'maxNONE' : -1,
+                          'maxTO': -1,
+                          'maxIN': -1,
+                          'maxPREP': -1}    
+        
+    def get_path(self,tree,slot):
+        pos, role, secondary, tertiary = slot      
+        heads = self.pos_map[pos]
+        if type(heads) == type({}):
+            heads = heads[role]
+        maxd = -1
+        for head in heads:            
+            if 'max'+head in self.depth_map:
+                maxd = self.depth_map['max'+head]                    
+        
+        mainpos = self.th.get_main_pos_path(tree,head,maxd)
+        print 'path to mainpos for slot(',slot,') ',mainpos
+        return mainpos 
+        
+    def match_subject(self,subframe,v,tree):
+        '''    Match the subject given the subframe([]), path(tup) and syntaxparse(Tree)
+                return [path] to each slot       
+            subject must be to the left of v from root 
+        ''' 
+        res = []
+        #rev = v.reverse()
+        nearestLeftBranch = self.th.nearest_left_sibling(v)       
+        for slot in subframe:            
+            res.append(self.get_path(tree,slot))
+        return res
+    
+    def match_object(self,subframe,v,tree):
+        '''    Match the object given the subframe([]), path(tup) and syntaxparse(Tree)
+                return [path] to each slot        
+            object must be to the right of v
+        ''' 
+        tree = copy.deepcopy(tree)
+        res = []       
+        nearestRightBranch = self.th.nearest_right_sibling(v,tree)
+        #nearest sibling methods return inclusive so we can pop greater than the last elmt
+        for i in nearestRightBranch[:-1]:
+            tree = tree.pop(i)
+        #At this point, tree should be the constituent VP of the main verb
+        next = nearestRightBranch[-1]+1
+        for slot in subframe:
+            if next < len(tree):
+                res.append(self.get_path(tree.pop(next),slot))
+            else:
+                raise SlotTreeCountError            
+        return res      
+        
+    def proximity_match_frame(self,v,center,frame,tree):
+        '''Given a v(tup) and center(int) match the frame around that path.'''        
+        left = frame[:center]#S
+        #V
+        right = frame[center+1:]#O
+        s = self.match_subject(left,v,tree)
+        o = self.match_object(right,v,tree)
+        return [s,v,o]
         
     def match_frame(self,frame,parse):
         '''Try to match the frame to the parse'''
-        try:
-            for slot in frame:
-                pos, role, secondary, tertiary = slot      
-                head = self.posmap[pos]
-                if type(head) == type({}):
-                    head = head[role]           
-                mainpos = self.th.get_main_pos_path(parse,head,self.maxVBDepth)
-                print 'path to mainpos for slot(',slot,') ',mainpos
-        except VBTooDeep:
-            sys.stderr.write("VB found but too deep given maxVBDepth\n")
+        try:            
+            verbslots = [(i,w) for i,w in enumerate(frame) if w[0] == "VERB"]            
+            if len(verbslots) > 1: raise VerbFrameCountError
+            verbindex, verbslot = verbslots[0]
+            verbpath = self.get_path(parse,verbslot)
+            match = self.proximity_match_frame(verbpath,verbindex,frame,parse)      
+            print 'Done matching slots in frame(',frame,')'              
+        except PosTooDeep, pos:
+            sys.stderr.write(pos+" found but too deep given max part-of-speech depth\n")
+        except VerbFrameCountError:
+            sys.stderr.write(str(frame)+" contains too many or too few verbs...error\n")
+        except SlotTreeCountError:
+            #Not all the slots could be filled for this frame given this parse
+            return None
         except AttributeError, e:
-            sys.stderr.write("Could not find the part of speech that you were looking for.\n")
+            print 'Attribute error trying to match frame: ',str(e)
+        return match
         
 
 if __name__=="__main__":
